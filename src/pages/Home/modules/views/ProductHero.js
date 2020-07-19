9f1a4c4b0ddc93ebcda660235bcfbfa13c7c3d14
ETomacheski <tomacheski2001@gmail.com>
2020-07-18 21:55:04 -0300

    arrumando botão das imagens

--- src/pages/Home/modules/views/ProductHero.js
+++ src/pages/Home/modules/views/ProductHero.js
@@ -1,36 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '../components/Button';
-import Typography from '../components/Typography';
-import ProductHeroLayout from './ProductHeroLayout';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '../components/Button'
+import Typography from '../components/Typography'
+import ProductHeroLayout from './ProductHeroLayout'
+import Grid from '@material-ui/core/Grid'
+import clsx from 'clsx'
 
 const backgroundImage =
-  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
+  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80'
 
 const styles = (theme) => ({
   background: {
     backgroundImage: `url(${backgroundImage})`,
     backgroundColor: '#7fc7d9', // Average color of the background image.
-    backgroundPosition: 'center',
+    backgroundPosition: 'center'
   },
   button: {
     minWidth: 200,
+    padding: '10px'
   },
   h5: {
     marginBottom: theme.spacing(4),
     marginTop: theme.spacing(4),
     [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing(10),
-    },
+      marginTop: theme.spacing(10)
+    }
   },
   more: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(2)
   },
-});
+  buttonColor: {
+    backgroundColor: '#DB87D5',
+    '&:hover': {
+      background: '#efefef',
+      color: '#000'
+    }
+  },
+  fontNunito: {
+    fontFamily: 'Nunito'
+  }
 
-function ProductHero(props) {
-  const { classes } = props;
+})
+
+function ProductHero (props) {
+  const { classes } = props
 
   return (
     <ProductHeroLayout backgroundClassName={classes.background}>
@@ -38,38 +52,28 @@ function ProductHero(props) {
       <img
         style={{ display: 'none' }}
         src={backgroundImage}
-        alt="increase priority"
+        alt='increase priority'
       />
-      <Typography color="inherit" align="center" variant="h2" marked="center">
-        Upgrade your Sundays
-      </Typography>
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
-        className={classes.h5}
-      >
-        Enjoy secret offers up to -70% off the best luxury hotels every Sunday.
-      </Typography>
-      <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        className={classes.button}
-        component="a"
-        href="/premium-themes/onepirate/sign-up/"
-      >
-        Register
-      </Button>
-      <Typography variant="body2" color="inherit" className={classes.more}>
-        Discover the experience
-      </Typography>
+
+      <Grid container spacing={2} justify='center'>
+        <Grid item>
+          <Button variant='contained' className={clsx(classes.buttonColor, classes.fontNunito)}>
+                    Escreva seu comercio
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant='outlined' className={clsx(classes.buttonColor, classes.fontNunito)}>
+                    Converse com a vovó
+          </Button>
+        </Grid>
+      </Grid>
+
     </ProductHeroLayout>
-  );
+  )
 }
 
 ProductHero.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(ProductHero);
+export default withStyles(styles)(ProductHero)
--- src/pages/Home/modules/views/ProductHeroLayout.js
+++ src/pages/Home/modules/views/ProductHeroLayout.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import { withStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 
 const styles = (theme) => ({
   root: {
@@ -13,15 +13,15 @@ const styles = (theme) => ({
     [theme.breakpoints.up('sm')]: {
       height: '80vh',
       minHeight: 500,
-      maxHeight: 1300,
-    },
+      maxHeight: 1300
+    }
   },
   container: {
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(14),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   backdrop: {
     position: 'absolute',
@@ -31,7 +31,7 @@ const styles = (theme) => ({
     bottom: 0,
     backgroundColor: theme.palette.common.black,
     opacity: 0.5,
-    zIndex: -1,
+    zIndex: -1
   },
   background: {
     position: 'absolute',
@@ -41,45 +41,33 @@ const styles = (theme) => ({
     bottom: 0,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    zIndex: -2,
+    zIndex: -2
   },
   arrowDown: {
     position: 'absolute',
-    bottom: theme.spacing(4),
-  },
-});
+    bottom: theme.spacing(4)
+  }
+})
 
-function ProductHeroLayout(props) {
-  const { backgroundClassName, children, classes } = props;
+function ProductHeroLayout (props) {
+  const { backgroundClassName, children, classes } = props
 
   return (
     <section className={classes.root}>
       <Container className={classes.container}>
-        <img
-          src="/static/themes/onepirate/productHeroWonder.png"
-          alt="wonder"
-          width="147"
-          height="80"
-        />
         {children}
         <div className={classes.backdrop} />
         <div className={clsx(classes.background, backgroundClassName)} />
-        <img
-          className={classes.arrowDown}
-          src="/static/themes/onepirate/productHeroArrowDown.png"
-          height="16"
-          width="12"
-          alt="arrow down"
-        />
+
       </Container>
     </section>
-  );
+  )
 }
 
 ProductHeroLayout.propTypes = {
   backgroundClassName: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(ProductHeroLayout);
+export default withStyles(styles)(ProductHeroLayout)
